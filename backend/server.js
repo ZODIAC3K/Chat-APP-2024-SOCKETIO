44f271754d78ee445e3ac438e9712d1422dc7baa
zodiac3k <harshdeepanshustrix@gmail.com>
2024-08-29 01:41:43 +0530

    added file structure

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+import express from "express"; // import express
+import { createServer } from "http"; // import http
+import { Server } from "socket.io"; // import socket.io
+// we will manage routes from express and socket connection using socket.io but since socket io needs a http server we pass the express app to the http server and then pass the http server to the socket io server
+
+const app = express(); // create an express app
+const server = createServer(app); // create a http server using the express app
+const io = new Server(server, {
+	cors: {
+		origin: "*",
+	},
+}); // create a socket io server using the http server which was created using the express app
+
+io.on("connection", (socket) => { // listen for connection event on the socket io server which is triggered when a client connects to the socket io server. we can even define a custom event name instead of connection but it spicifically have to trigger this from the client side using the same event name.
+    
+    console.log("User Connected and Socket ID: ", socket.id);
+
+    socket.on("chat", (payload) => {
+        console.log("Data: ", payload);
+        io.emit("chat", payload);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("User Disconnected and Socket ID: ", socket.id);
+    });
+});
+
+server.listen(5000, () => {
+	console.log("Server is running on port:", 5000);
+});
